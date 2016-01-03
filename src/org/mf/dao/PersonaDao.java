c15fbeb17e4529a6b36b2b464cf923d240f1c926
Mauro Fugante <mauro.fugante@gmail.com>
2016-01-03 12:18:07 +0100

    gestito i valori null da mettere sul db

--- src/org/mf/dao/PersonaDao.java
+++ src/org/mf/dao/PersonaDao.java
@@ -1,33 +1,35 @@
 package org.mf.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.mf.model.Persona;
-import org.mf.util.Database;
 
-
-public class PersonaDao {
-
-	private Connection connection;
+public class PersonaDao extends Dao {
+	
+	private static final String TABLE_NAME = "persona";
 
 	public PersonaDao() {
-		connection = Database.getConnection();
+		super();
+	}
+	
+	@Override
+	String getTableName() {
+		return TABLE_NAME;
 	}
 
 	public void check(Persona persona) {
 		try {
-			PreparedStatement ps = connection.prepareStatement("select nome from persona where id = ?");
+			PreparedStatement ps = getConnection().prepareStatement("select nome from persona where id = ?");
 			ps.setInt(1, persona.getId());
 			ResultSet rs = ps.executeQuery();
-			if (rs.next()) // found
-			{
-				update(persona);
+			if (rs.next()) {
+				update(persona);	
 			} else {
 				add(persona);
 			}
@@ -45,22 +47,21 @@ public class PersonaDao {
 		//          1     2        3      4     5          6            7     8        9     0       1      2        3
 
 		try {
-			PreparedStatement stmt = connection.prepareStatement(sb.toString());
-			// Parameters start with 1
+			PreparedStatement stmt = getConnection().prepareStatement(sb.toString());
 			
-			stmt.setString(1, persona.getNome());
-			stmt.setString(2, persona.getCognome());
-			stmt.setString(3, persona.getCitta());
-			stmt.setString(4, persona.getProv());
-			stmt.setString(5, persona.getIndirizzo());
-			stmt.setString(6, persona.getTelefono());
-			stmt.setString(7, persona.getMail());
-			stmt.setString(8, persona.getCodFisc() );
-			stmt.setString(9, persona.getPsw());
-			stmt.setString(10, persona.getUtente());
-			stmt.setString(11, persona.getRuolo().toString());
-			stmt.setDate(12, new java.sql.Date(persona.getNascita().getTime()));
-			stmt.setString(13, persona.getSesso().toString());
+			stmtPara(stmt, 1, Types.VARCHAR, persona.getNome());
+			stmtPara(stmt, 2, Types.VARCHAR, persona.getCognome());
+			stmtPara(stmt, 3, Types.VARCHAR, persona.getCitta());
+			stmtPara(stmt, 4, Types.VARCHAR, persona.getProv());
+			stmtPara(stmt, 5, Types.VARCHAR, persona.getIndirizzo());
+			stmtPara(stmt, 6, Types.VARCHAR, persona.getTelefono());
+			stmtPara(stmt, 7, Types.VARCHAR, persona.getMail());
+			stmtPara(stmt, 8, Types.VARCHAR, persona.getCodFisc() );
+			stmtPara(stmt, 9, Types.VARCHAR, persona.getPsw());
+			stmtPara(stmt, 10, Types.VARCHAR, persona.getUtente());
+			stmtPara(stmt, 11, Types.VARCHAR, persona.getRuolo().toString());
+			stmtPara(stmt, 12, Types.DATE, new java.sql.Date(persona.getNascita().getTime()));
+			stmtPara(stmt, 13, Types.VARCHAR, persona.getSesso().toString());
 
 			stmt.executeUpdate();
 
@@ -68,23 +69,7 @@ public class PersonaDao {
 			e.printStackTrace();
 		}
 	}
-
-	public void delete(Integer id) {
-		
-		if (id == null || id == 0)
-			return;
-		
-		try {
-			PreparedStatement preparedStatement = connection.prepareStatement("delete from persona where id=?");
-			// Parameters start with 1
-			preparedStatement.setInt(1, id);
-			preparedStatement.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
+	
 	public void update(Persona persona) {
 		
 		StringBuffer sb = new StringBuffer();
@@ -107,26 +92,24 @@ public class PersonaDao {
 		
 		try {
 			
-			PreparedStatement preparedStatement = connection.prepareStatement(sb.toString());
-			// Parameters start with 1
-//			System.out.println(new java.sql.Date(persona.getRegisteredon().getTime()));
+			PreparedStatement stmt = getConnection().prepareStatement(sb.toString());
 			
-			preparedStatement.setString(1, persona.getNome());
-			preparedStatement.setString(2, persona.getCognome());
-			preparedStatement.setString(3, persona.getCitta());
-			preparedStatement.setString(4, persona.getProv());
-			preparedStatement.setString(5, persona.getIndirizzo());
-			preparedStatement.setString(6, persona.getTelefono());
-			preparedStatement.setString(7, persona.getMail());
-			preparedStatement.setString(8, persona.getCodFisc());
-			preparedStatement.setString(9, persona.getPsw());
-			preparedStatement.setString(10, persona.getUtente());
-			preparedStatement.setString(11, persona.getRuolo());
-			preparedStatement.setDate(12, new java.sql.Date(persona.getNascita().getTime()));
-			preparedStatement.setString(13, persona.getSesso());
-			preparedStatement.setInt(14, persona.getId());
+			stmtPara(stmt, 1, Types.VARCHAR, persona.getNome());
+			stmtPara(stmt, 2, Types.VARCHAR, persona.getCognome());
+			stmtPara(stmt, 3, Types.VARCHAR, persona.getCitta());
+			stmtPara(stmt, 4, Types.VARCHAR, persona.getProv());
+			stmtPara(stmt, 5, Types.VARCHAR, persona.getIndirizzo());
+			stmtPara(stmt, 6, Types.VARCHAR, persona.getTelefono());
+			stmtPara(stmt, 7, Types.VARCHAR, persona.getMail());
+			stmtPara(stmt, 8, Types.VARCHAR, persona.getCodFisc());
+			stmtPara(stmt, 9, Types.VARCHAR, persona.getPsw());
+			stmtPara(stmt, 10, Types.VARCHAR, persona.getUtente());
+			stmtPara(stmt, 11, Types.VARCHAR, persona.getRuolo());
+			stmtPara(stmt, 12, Types.DATE, new java.sql.Date(persona.getNascita().getTime()));
+			stmtPara(stmt, 13, Types.VARCHAR, persona.getSesso());
+			stmtPara(stmt, 14, Types.INTEGER, persona.getId());
 			
-			preparedStatement.executeUpdate();
+			stmt.executeUpdate();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -135,8 +118,9 @@ public class PersonaDao {
 
 	public List<Persona> getAll() {
 		List<Persona> retValue = new ArrayList<Persona>();
+
 		try {
-			Statement statement = connection.createStatement();
+			Statement statement = getConnection().createStatement();
 			ResultSet rs = statement.executeQuery("select * from persona");
 			while (rs.next()) {
 				retValue.add(assignBean(rs));
@@ -149,23 +133,10 @@ public class PersonaDao {
 	}
 
 	public Persona getById(int id) {
-		Persona persona = new Persona();
-		try {
-			PreparedStatement preparedStatement = connection.prepareStatement("select * from persona where id=?");
-			preparedStatement.setInt(1, id);
-			ResultSet rs = preparedStatement.executeQuery();
-
-			if (rs.next()) {
-				persona = assignBean(rs);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return persona;
+		return (Persona)super.getById(id);
 	}
 	
-	private Persona assignBean(ResultSet rs) throws SQLException {
+	Persona assignBean(ResultSet rs) throws SQLException {
 		Persona retValue = new Persona();
 		retValue.setId(rs.getInt("id"));
 		retValue.setNome(rs.getString("nome"));
@@ -183,4 +154,5 @@ public class PersonaDao {
 		retValue.setSesso(rs.getString("sesso"));
 		return retValue;
 	}
+
 }
