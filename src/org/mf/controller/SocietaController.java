edb7d8116e840068d19bc99b5a4a36a0ca1bb087
Mauro Fugante <mauro.fugante@gmail.com>
2016-01-12 09:45:23 +0100

    CRUD

--- src/org/mf/controller/SocietaController.java
+++ src/org/mf/controller/SocietaController.java
@@ -1,96 +1,100 @@
 package org.mf.controller;
 
-class SocietaController {
+import java.io.IOException;
 
-//    def index(Integer max) {
-//        params.max = Math.min(max ?: 10, 100)
-//        respond Societa.list(params), model:[societaInstanceCount: Societa.count()]
-//    }
-//
-//    def show(Societa societaInstance) {
-//        respond societaInstance
-//    }
-//
-//    def create() {
-//        respond new Societa(params)
-//    }
-//
-//    @Transactional
-//    def save(Societa societaInstance) {
-//        if (societaInstance == null) {
-//            notFound()
-//            return
-//        }
-//
-//        if (societaInstance.hasErrors()) {
-//            respond societaInstance.errors, view:'create'
-//            return
-//        }
-//
-//        societaInstance.save flush:true
-//
-//        request.withFormat {
-//            form multipartForm {
-//                flash.message = message(code: 'default.created.message', args: [message(code: 'societa.label', default: 'Societa'), societaInstance.id])
-//                redirect societaInstance
-//            }
-//            '*' { respond societaInstance, [status: CREATED] }
-//        }
-//    }
-//
-//    def edit(Societa societaInstance) {
-//        respond societaInstance
-//    }
-//
-//    @Transactional
-//    def update(Societa societaInstance) {
-//        if (societaInstance == null) {
-//            notFound()
-//            return
-//        }
-//
-//        if (societaInstance.hasErrors()) {
-//            respond societaInstance.errors, view:'edit'
-//            return
-//        }
-//
-//        societaInstance.save flush:true
-//
-//        request.withFormat {
-//            form multipartForm {
-//                flash.message = message(code: 'default.updated.message', args: [message(code: 'Societa.label', default: 'Societa'), societaInstance.id])
-//                redirect societaInstance
-//            }
-//            '*'{ respond societaInstance, [status: OK] }
-//        }
-//    }
-//
-//    @Transactional
-//    def delete(Societa societaInstance) {
-//
-//        if (societaInstance == null) {
-//            notFound()
-//            return
-//        }
-//
-//        societaInstance.delete flush:true
-//
-//        request.withFormat {
-//            form multipartForm {
-//                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Societa.label', default: 'Societa'), societaInstance.id])
-//                redirect action:"index", method:"GET"
-//            }
-//            '*'{ render status: NO_CONTENT }
-//        }
-//    }
-//
-//    protected void notFound() {
-//        request.withFormat {
-//            form multipartForm {
-//                flash.message = message(code: 'default.not.found.message', args: [message(code: 'societa.label', default: 'Societa'), params.id])
-//                redirect action: "index", method: "GET"
-//            }
-//            '*'{ render status: NOT_FOUND }
-//        }
-//    }
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.mf.dao.SocietaDao;
+import org.mf.model.Societa;
+import org.mf.util.Utility;
+
+/**
+ * Servlet implementation class SocietaController
+ */
+@WebServlet("/SocietaController")
+public class SocietaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static String INSERT_OR_EDIT = "/societa.jsp";
+	private static String LIST = "/listsocieta.jsp";
+	private SocietaDao dao;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SocietaController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String forward = "";
+		String action = request.getParameter("action");
+		if (action == null)
+			action = "list";
+
+		if (action.equalsIgnoreCase("edit")) {
+			Integer id = getId(request);
+			Societa societa = dao.getById(id);
+			forward = INSERT_OR_EDIT;
+			request.setAttribute("bean", societa);
+		} else if (action.equalsIgnoreCase("delete")) {
+			Integer id = getId(request);
+			dao.delete(id);
+			forward = LIST;
+			request.setAttribute("beans", dao.getAll());
+		} else if (action.equalsIgnoreCase("list")) {
+			forward = LIST;
+			request.setAttribute("beans", dao.getAll());
+		} else {
+			forward = INSERT_OR_EDIT;
+		}
+
+		RequestDispatcher view = request.getRequestDispatcher(forward);
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+Integer id = getId(request);
+		
+		Societa retValue = new Societa();
+		
+		retValue.setId(id);
+		retValue.setNome(request.getParameter("nome"));
+		retValue.setCitta(request.getParameter("citta"));
+		retValue.setProv(request.getParameter("prov"));
+		retValue.setIndirizzo(request.getParameter("indirizzo"));
+		retValue.setCodiceFederale(request.getParameter("codiceFederale"));
+		//retValue.setGiorniRitardoAmmesso(request.getParameter("giorniRitardoAmmesso"));
+		retValue.setSite(request.getParameter("site"));
+		retValue.setMail(request.getParameter("mail"));
+		//retValue.setPersona(request.getParameter("persona"));
+		
+		if (id == null || id == 0) {
+			dao.add(retValue);
+		} else {
+			dao.check(retValue);
+		}
+		
+		RequestDispatcher view = request.getRequestDispatcher(LIST);
+		request.setAttribute("beans", dao.getAll());
+		view.forward(request, response);
+	}
+	
+	private Integer getId(HttpServletRequest request) {
+		return Utility.getInteger(request.getParameter("id"));
+	}
+
+
 }
